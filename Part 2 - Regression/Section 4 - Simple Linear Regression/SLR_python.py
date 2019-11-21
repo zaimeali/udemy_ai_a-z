ee2f6cf4475a9c94fb9ebda4ae1406f927ba0869
Zaime Ali <zaimeali1997@gmail.com>
2019-11-21 19:54:56 +0500

    SLR in Python - Step 2 Complete

--- Part 2 - Regression/Section 4 - Simple Linear Regression/SLR_python.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/SLR_python.py
@@ -15,3 +15,11 @@ y = dataset.iloc[:, -1].values
 # Split the dataset into the training and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+
+# Fitting SLR to the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting
