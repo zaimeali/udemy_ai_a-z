f711d8698a18c284ada1b87863ecac113af6dda5
Zaime Ali <zaimeali1997@gmail.com>
2019-11-21 19:47:19 +0500

    SLR in Python - Step 1 Complete

--- Part 2 - Regression/Section 4 - Simple Linear Regression/SLR_python.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/SLR_python.py
@@ -1,7 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 21 19:21:52 2019
+# Simple Linear Regression
 
-@author: zaime
-"""
+#Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+
+# Importing the Dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+
+# Split the dataset into the training and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
