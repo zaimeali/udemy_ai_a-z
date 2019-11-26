26449ec9a9d4dd8d176e2eaba502745eb136598c
Zaime Ali <zaimeali1997@gmail.com>
2019-11-26 21:49:53 +0500

    Will Start MLR

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/MLR.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/MLR.py
@@ -2,17 +2,5 @@
 
 # y = b0 + b1*X1 + b2*X2+.....+bn*Xn
 
-# Do not add all the dummy variables
-# D2 = 1 - D1 
-# Always omit one dummmy variable
-# if it has 100 dummy variables include 99
+# Must read Help_MLR.txt
 
-# P-Value: is a statistical measure that helps scientists determine whether or not their hypotheses are correct
-# P Values are used to determine whether the results of their experiment are within the normal range of values for 
-# 	      the events being observed. 
-# Usually, if the P value of a data set is below a certain pre-determined amount (like, for instance, 0.05), 
-# 	scientists will reject the "null hypothesis" of their experiment - in other words, 
-# 	they'll rule out the hypothesis that the variables of their experiment had no meaningful effect on the results
-
-# The p-value is NOT the probability the claim is true.
-# The p-value is NOT the probability the null hypothesis is true.
