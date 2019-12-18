e5877b9bbb23ce6b0c385972ba57b4aee4240002
Zaime Ali <zaimeali1997@gmail.com>
2019-12-18 21:26:46 +0500

    SVR in Python

--- Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/SVR.py
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/SVR.py
@@ -29,3 +29,38 @@ closest to our test evaluation location.
 '''
 
 
+# Import Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Import Dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X = sc_X.fit_transform(X)
+sc_y = StandardScaler()
+
+y = sc_y.fit_transform(pd.DataFrame(y))
+
+# SVR 
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
+
+# predictor
+y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
+
+# Visualize
+plt.scatter(X, y, color='red')
+plt.plot(X, regressor.predict(X), color='blue')
+plt.title("Truth or Bluff")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
+
+
